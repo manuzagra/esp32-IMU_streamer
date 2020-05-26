a6d620838c7419098dbbce813877c389448a5f11
manuzagra <manuzagra@gmail.com>
2020-05-26 21:12:50 +0200

    insert, find, update, delete methods added to the jsondatabas. The new methods are not tested

--- jsondb/jsondb.py
+++ jsondb/jsondb.py
@@ -3,6 +3,18 @@ import ujson
 
 import utils
 
+
+# collection =
+# {
+# 0: {'ssid': 'asdasd', 'password': 'jujujo'}
+# 1: {'ssid': 'asdasd', 'password': 'jujujo', 'autoconnect': False}
+# 2: {'ssid': 'asdasd', 'password': 'jujujo'}
+# 3: {'ssid': 'asdasd', 'password': 'jujujo', 'autoconnect': True}
+# 4: {'ssid': 'asdasd', 'password': 'jujujo', 'name': 'house'}
+# }
+
+
+
 class JSONDB:
     def __init__(self, path):
         # the database is going to be a directory with files inside
@@ -18,7 +30,7 @@ class JSONDB:
                     os.chdir(d)
                 except:
                     pass
-        os.chdir('/')
+            os.chdir('/')
         if not utils.is_dir(path):
             raise Exception('JSONDB cannot create the structure of directories')
 
@@ -33,7 +45,9 @@ class JSONDB:
             self._cache[collection] = ujson.load(f)
 
     def clear_cache(self, collections=None):
-        pass
+        self.flush(collections)
+        for coll in list(collections):
+            del self._cache[coll]
 
     def flush(self, collections=None):
         if collections is None:
@@ -42,6 +56,48 @@ class JSONDB:
             with open(self._abs_path(collection), 'w') as f:
                 ujson.dump(self._cache[collection], f)
 
-    def create_collection(self, name):
+    def create_collection(self, collection, cache=True):
         # a collection is gonna be a new file
-        pass
+        with open(self._abs_path(collection), 'w') as f:
+            pass
+        if cache:
+            self._cache[collection] = {'next_id': 0}
+
+    def drop_collection(self, collection):
+        self.clear_cache(collection)
+        or.remove(self._abs_path(collection))
+
+    def insert(self, collection, document):
+        self._cache[collection][self._cache[collection][next_id]] = document
+        self._cache[collection][next_id] += 1
+
+    # this decorator convert expresion into a exception free function
+    # it is needed because no all the documents contain the same information
+    def _safe_expression(exp):
+        def s_exp(*args, **kwargs):
+            try:
+                return exp(*args, **kwargs)
+            except:
+                return False
+        return s_exp
+
+    def find(self, collection, expression, fields=None):
+        expression = self._safe_expression(expression)
+        if fields:
+            def get_fields(doc):
+                available_fields = set(fields).intersection(set(doc.keys()))
+                return {k:v for k, v in doc.items() if k in available_fields}
+        else:
+            get_fields = lambda doc: doc
+        return [get_fields(doc) for id, doc in self._cache[collection].items() if expression(doc)]
+
+    def update(self, collection, selection_criteria, update_function):
+        selection_criteria = self._safe_expression(selection_criteria)
+        update_function = self._safe_expression(update_function)
+        [update_function(d) for id, doc in self._cache[collection].items() if selection_criteria(doc)]
+
+    def delete(self, collection, expression):
+        def delete_doc(id):
+            del self._cache[collection][id]
+        temp = self._cache[collection].items()
+        [delete_doc(id) for id, doc in temp if expression(doc)]
