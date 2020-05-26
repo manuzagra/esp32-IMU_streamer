7e28cd08230cc825b561da786560a1fb07d4078b
manuzagra <manuzagra@gmail.com>
2020-05-26 02:01:26 +0200

    getting the ftp server running at statup

--- boot.py
+++ boot.py
@@ -4,9 +4,12 @@
 #import webrepl
 #webrepl.start()
 
-
+import utils
 import connections
 
 wifi_station = connections.wifi.Station()
 wifi_station.connect('Cebolleta', 'tiguer32')
 wifi_station.set_ip('192.168.0.40')
+
+
+import utils.uftpd
