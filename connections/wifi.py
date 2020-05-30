d4adf195ecdffab3228da38d4cb7ccd6e42968b3
manuzagra <manuzagra@gmail.com>
2020-05-30 03:16:24 +0200

    small modifications to use the station and the acces point to set up the sensor

--- connections/wifi.py
+++ connections/wifi.py
@@ -1,53 +1,61 @@
 import network
+import utime
 
 # https://docs.micropython.org/en/latest/library/network.WLAN.html
 
 class Station:
     def __init__(self):
-        self._conn = network.WLAN(network.STA_IF)
+        self._wlan = network.WLAN(network.STA_IF)
 
     def activate(self):
-        self._conn.active(True)
+        self._wlan.active(True)
 
     def deactivate(self):
-        self._conn.active(False)
+        self._wlan.active(False)
 
-    def connect(self, essid, password):
-        self._conn.active(True)
-        self._conn.connect(essid, password)
-        while not self._conn.isconnected():
-            pass
+    def connect(self, essid, password, timeout=0):
+        self._wlan.active(True)
+        self._wlan.connect(essid, password)
+        for t in range(timeout):
+            utime.sleep(1)
+            if self._wlan.isconnected():
+                return True
+        return False
 
     def disconnect(self):
-        self._conn.disconnect()
+        self._wlan.disconnect()
 
-    def get_connection(self):
-        return self._conn
+    def get_wlanection(self):
+        return self._wlan
 
     def set_ip(self, ip):
-        _ip = list(self._conn.ifconfig())
+        _ip = list(self._wlan.ifconfig())
         _ip[0] = ip
-        self._conn.ifconfig(tuple(_ip))
+        self._wlan.ifconfig(tuple(_ip))
+
+    def scan(self):
+        return self._wlan.scan()
 
 
 class AccessPoint:
     def __init__(self):
-        self._conn = network.WLAN(network.AP_IF)
+        self._wlan = network.WLAN(network.AP_IF)
 
     def activate(self):
-        self._conn.active(True)
+        self._wlan.active(True)
 
     def deactivate(self):
-        self._conn.active(False)
+        self._wlan.active(False)
 
     def connect(self, essid, password):
-        self._conn.active(True)
-        self._conn.config(essid=essid, password=password)
-        while not self._conn.isconnected():
+        self._wlan.active(True)
+        self._wlan.config(essid=essid, password=password)
+        while not self._wlan.isconnected():
             pass
+        return True
 
     def disconnect(self):
-        self._conn.disconnect()
+        self._wlan.disconnect()
 
-    def get_connection(self):
-        return self._conn
+    def get_wlanection(self):
+        return self._wlan
